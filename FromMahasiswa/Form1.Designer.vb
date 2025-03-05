592342323922478b0f18499781427fd35d3dd30e
anantadhua <anantadhuaheryanty@gmail.com>
2025-03-05 23:11:43 +0700

    Tugas

---
+++ FromMahasiswa/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtnip = New System.Windows.Forms.TextBox()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cbfakultas = New System.Windows.Forms.ComboBox()
+        Me.cbjurusan = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnprocess = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtnip
+        '
+        Me.txtnip.Location = New System.Drawing.Point(186, 82)
+        Me.txtnip.Name = "txtnip"
+        Me.txtnip.Size = New System.Drawing.Size(211, 26)
+        Me.txtnip.TabIndex = 0
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(186, 126)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(289, 26)
+        Me.txtnama.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(113, 88)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "NIP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(113, 132)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nama"
+        '
+        'cbfakultas
+        '
+        Me.cbfakultas.FormattingEnabled = True
+        Me.cbfakultas.Location = New System.Drawing.Point(186, 169)
+        Me.cbfakultas.Name = "cbfakultas"
+        Me.cbfakultas.Size = New System.Drawing.Size(240, 28)
+        Me.cbfakultas.TabIndex = 4
+        '
+        'cbjurusan
+        '
+        Me.cbjurusan.FormattingEnabled = True
+        Me.cbjurusan.Location = New System.Drawing.Point(186, 218)
+        Me.cbjurusan.Name = "cbjurusan"
+        Me.cbjurusan.Size = New System.Drawing.Size(271, 28)
+        Me.cbjurusan.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(113, 177)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Fakultas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(113, 226)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Jurusan"
+        '
+        'btnprocess
+        '
+        Me.btnprocess.Location = New System.Drawing.Point(253, 263)
+        Me.btnprocess.Name = "btnprocess"
+        Me.btnprocess.Size = New System.Drawing.Size(103, 38)
+        Me.btnprocess.TabIndex = 8
+        Me.btnprocess.Text = "Process"
+        Me.btnprocess.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(626, 395)
+        Me.Controls.Add(Me.btnprocess)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.cbjurusan)
+        Me.Controls.Add(Me.cbfakultas)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.txtnip)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtnip As System.Windows.Forms.TextBox
+    Friend WithEvents txtnama As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cbfakultas As System.Windows.Forms.ComboBox
+    Friend WithEvents cbjurusan As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnprocess As System.Windows.Forms.Button
+
+End Class
---
+++ FromMahasiswa/Form2.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnprocess = New System.Windows.Forms.Button()
+        Me.rbP = New System.Windows.Forms.RadioButton()
+        Me.rbL = New System.Windows.Forms.RadioButton()
+        Me.cbjurusan = New System.Windows.Forms.ComboBox()
+        Me.cbfakultas = New System.Windows.Forms.ComboBox()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.txtnip = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnprocess
+        '
+        Me.btnprocess.Location = New System.Drawing.Point(155, 310)
+        Me.btnprocess.Name = "btnprocess"
+        Me.btnprocess.Size = New System.Drawing.Size(93, 38)
+        Me.btnprocess.TabIndex = 48
+        Me.btnprocess.Text = "Process"
+        Me.btnprocess.UseVisualStyleBackColor = True
+        '
+        'rbP
+        '
+        Me.rbP.AutoSize = True
+        Me.rbP.Location = New System.Drawing.Point(491, 161)
+        Me.rbP.Name = "rbP"
+        Me.rbP.Size = New System.Drawing.Size(116, 24)
+        Me.rbP.TabIndex = 47
+        Me.rbP.TabStop = True
+        Me.rbP.Text = "Perempuan"
+        Me.rbP.UseVisualStyleBackColor = True
+        '
+        'rbL
+        '
+        Me.rbL.AutoSize = True
+        Me.rbL.Location = New System.Drawing.Point(305, 161)
+        Me.rbL.Name = "rbL"
+        Me.rbL.Size = New System.Drawing.Size(96, 24)
+        Me.rbL.TabIndex = 46
+        Me.rbL.TabStop = True
+        Me.rbL.Text = "Laki Laki"
+        Me.rbL.UseVisualStyleBackColor = True
+        '
+        'cbjurusan
+        '
+        Me.cbjurusan.FormattingEnabled = True
+        Me.cbjurusan.Location = New System.Drawing.Point(305, 255)
+        Me.cbjurusan.Name = "cbjurusan"
+        Me.cbjurusan.Size = New System.Drawing.Size(284, 28)
+        Me.cbjurusan.TabIndex = 45
+        '
+        'cbfakultas
+        '
+        Me.cbfakultas.FormattingEnabled = True
+        Me.cbfakultas.Location = New System.Drawing.Point(305, 205)
+        Me.cbfakultas.Name = "cbfakultas"
+        Me.cbfakultas.Size = New System.Drawing.Size(178, 28)
+        Me.cbfakultas.TabIndex = 44
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(305, 113)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(284, 26)
+        Me.txtnama.TabIndex = 43
+        '
+        'txtnip
+        '
+        Me.txtnip.Location = New System.Drawing.Point(305, 69)
+        Me.txtnip.Name = "txtnip"
+        Me.txtnip.Size = New System.Drawing.Size(226, 26)
+        Me.txtnip.TabIndex = 42
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(118, 263)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 20)
+        Me.Label5.TabIndex = 41
+        Me.Label5.Text = "Jurusan"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(118, 213)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 20)
+        Me.Label4.TabIndex = 40
+        Me.Label4.Text = "Fakultas"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(118, 165)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 20)
+        Me.Label3.TabIndex = 39
+        Me.Label3.Text = "Jenis Kelamin"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(118, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 38
+        Me.Label2.Text = "Nama"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(118, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 20)
+        Me.Label1.TabIndex = 37
+        Me.Label1.Text = "NIP"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(725, 416)
+        Me.Controls.Add(Me.btnprocess)
+        Me.Controls.Add(Me.rbP)
+        Me.Controls.Add(Me.rbL)
+        Me.Controls.Add(Me.cbjurusan)
+        Me.Controls.Add(Me.cbfakultas)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.txtnip)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnprocess As System.Windows.Forms.Button
+    Friend WithEvents rbP As System.Windows.Forms.RadioButton
+    Friend WithEvents rbL As System.Windows.Forms.RadioButton
+    Friend WithEvents cbjurusan As System.Windows.Forms.ComboBox
+    Friend WithEvents cbfakultas As System.Windows.Forms.ComboBox
+    Friend WithEvents txtnama As System.Windows.Forms.TextBox
+    Friend WithEvents txtnip As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
